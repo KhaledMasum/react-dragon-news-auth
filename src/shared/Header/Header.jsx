b5055eccf52c0eeedefe6aff08ae1e254973aeb1
Khaled Masum <khaledmasum1@gmail.com>
2024-07-13 00:07:42 +0600

    Created some pages

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,7 +1,12 @@
+import logo from '../../assets/logo.png';
+import moment from 'moment';
+
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
+        <div className='text-center'>
+            <img src={logo} alt="" className='mx-auto' />
+            <p>Journalism Without Fear or Favour</p>
+            <p>{moment().format('dddd, MMMM DD, YYYY')}</p>
         </div>
     );
 };
--- src/shared/RightSideNav/RightSideNav.jsx
+++ src/shared/RightSideNav/RightSideNav.jsx
@@ -1,7 +1,43 @@
+import { FaFacebook, FaGoogle, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
+import qZone1 from '../../assets/qZone1.png';
+import qZone2 from '../../assets/qZone2.png';
+import qZone3 from '../../assets/qZone3.png';
+
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-2xl">Right side</h2>
+            <div className="p-4 space-y-3 mb-4">
+                <h2 className="text-3xl">Login With</h2>
+                <button className="btn btn-outline w-full">
+                    <FaGoogle />
+                    Login With Google
+                </button>
+                <button className="btn btn-outline w-full">
+                    <FaGithub />
+                    Login With Github
+                </button>
+            </div>
+            <div className="p-4 mb-4">
+                <h2 className="text-3xl mb-2">Find us on</h2>
+                <a href="" className="p-4 flex text-lg items-center border rounded-t-lg">
+                    <FaFacebook className="mr-3" />
+                    <span>Facebook</span>
+                </a>
+                <a href="" className="p-4 flex text-lg items-center border-x">
+                    <FaTwitter className="mr-2" />
+                    <span>Twitter</span>
+                </a>
+                <a href="" className="p-4 flex text-lg items-center border rounded-b-lg">
+                    <FaInstagram className="mr-2" />
+                    <span>Instagram</span>
+                </a>
+            </div>
+            <div className="p-4 space-y-3 mb-4">
+                <h2 className="text-3xl">Q-Zone</h2>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+            </div>
         </div>
     );
 };
